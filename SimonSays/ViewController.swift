0681ec94c85cb09bbb21c2e9a8954c2234149235
Morgan Hondros <morganhondros@gmail.com>
2019-06-24 19:03:31 -0400

    end of lecture 8.

--- SimonSays/ViewController.swift
+++ SimonSays/ViewController.swift
@@ -15,6 +15,12 @@ class ViewController: UIViewController {
     @IBOutlet var playerLabels: [UILabel]!
     @IBOutlet var scoreLabels: [UILabel]!
     
+    var currentPlayer = 0
+    var scores = [0, 0]
+    var sequenceIndex = 0
+    var colorSequence = [Int]()
+    var colorsToTap = [Int]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         colorButtons = colorButtons.sorted() {
@@ -32,18 +38,55 @@ class ViewController: UIViewController {
     }
     
     func createNewGame() {
+        colorSequence.removeAll()
         actionButton.setTitle("Start Game", for: .normal)
+        actionButton.isEnabled = true
         for button in colorButtons {
             button.alpha = 0.5
+            button.isEnabled = false
         }
     }
+    
+    func addNewColor() {
+        colorSequence.append(Int(arc4random_uniform(UInt32(4))))
+        
+        
+    }
+    
+    func playSequence() {
+        if sequenceIndex < colorSequence.count {
+            flash(button: colorButtons[colorSequence[sequenceIndex]])
+            sequenceIndex += 1
+        } else {
+            colorsToTap = colorSequence
+            view.isUserInteractionEnabled = true
+            actionButton.setTitle("Tap the Circle", for: .normal)
+        }
+    }
+    
+    func flash(button: CircularButton) {
+        UIView.animate(withDuration: 0.5, animations: {
+            button.alpha = 1.0
+            button.alpha = 0.5
+        }) { (bool) in
+            self.playSequence()
+        }
+    }
+    
+    
 
     @IBAction func colorButtonHandler(_ sender: CircularButton) {
         print("Button \(sender.tag) tapped")
     }
     @IBAction func actionButtonHandler(_ sender: UIButton) {
-        print("Action Button Tapped")
-        
+        sequenceIndex = 0
+        actionButton.setTitle("Memorize", for: .normal)
+        actionButton.isEnabled = false
+        view.isUserInteractionEnabled = false
+        addNewColor()
+        DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(1)) {
+            self.playSequence()
+        }
     }
     
 }
