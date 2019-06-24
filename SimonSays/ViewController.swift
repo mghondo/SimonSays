fd3df49ec9ae8e623ed532ad8a77249f8056ccd0
Morgan Hondros <morganhondros@gmail.com>
2019-06-23 21:27:21 -0400

    Initial Commit

---
+++ SimonSays/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimonSays
+//
+//  Created by Morgan Hondros on 6/23/19.
+//  Copyright © 2019 Morgan Hondros. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
